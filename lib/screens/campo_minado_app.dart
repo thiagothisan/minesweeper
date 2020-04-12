9abde33b801e9d564b4ee928ccc45665144976a8
thisan <thiago.thisan@gmail.com>
2020-04-12 12:28:54 -0300

    criacao do componente tabuleito_widget

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,22 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:minesweeper/components/campo_widget.dart';
 import 'package:minesweeper/components/resultado_widget.dart';
+import 'package:minesweeper/models/campo.dart';
+import 'package:minesweeper/models/explosao_exception.dart';
 
 class CampoMinadoApp extends StatelessWidget {
-
-  _reiniciar(){
+  void _reiniciar() {
     print('reiniciar...');
+  }
+
+  void _abrir(Campo campo) {
+    print('Abrir...');
+  }
+
+  void _alternarMarcacao(Campo campo) {
+    print('Alternar marcacao...');
   }
 
   @override
   Widget build(BuildContext context) {
+    Campo vizinho1 = Campo(linha: 1, coluna: 0);
+    vizinho1.minar();
+
+    Campo campo = Campo(linha: 0, coluna: 0);
+    campo.addvizinhos(vizinho1);
+
+    try {
+      //campo.minar();
+     // campo.abrir();
+     campo.alternarMarcacao();
+    } on ExplosaoException {}
+
     return MaterialApp(
       home: Scaffold(
-        appBar:ResultadoWidget(
+        appBar: ResultadoWidget(
           venceu: true,
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: Text('Tabuleiro'),
+          child: CampoWidget(
+            campo: campo,
+            onAbrir: _abrir,
+            onAlternarMarcacao: _alternarMarcacao,
+          ),
         ),
       ),
     );

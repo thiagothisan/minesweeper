e45e7bae0918c10e4d14284e38a8b9a42dab2909
thisan <thiago.thisan@gmail.com>
2020-04-12 13:05:28 -0300

    finalizando app

--- lib/components/tabuleiro_widget.dart
+++ lib/components/tabuleiro_widget.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:minesweeper/components/campo_widget.dart';
+import 'package:minesweeper/models/campo.dart';
+import 'package:minesweeper/models/tabuleiro.dart';
 
 class TabuleiroWidget extends StatelessWidget {
+  final Tabuleiro tabuleiro;
+  final void Function(Campo) onAbrir;
+  final void Function(Campo) onAlternarMarcacao;
+
+  TabuleiroWidget({
+    @required this.tabuleiro,
+    @required this.onAbrir,
+    @required this.onAlternarMarcacao,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: GridView.count(
+        crossAxisCount: tabuleiro.colunas,
+        children: tabuleiro.campos.map((c) {
+          return CampoWidget(
+            campo: c,
+            onAbrir: onAbrir,
+            onAlternarMarcacao: onAlternarMarcacao,
+          );
+        }).toList(),
+      ),
     );
   }
 }

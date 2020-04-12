d5acd5ae180b39b7a38d24193467dc8079966f7e
thisan <thiago.thisan@gmail.com>
2020-04-12 11:17:31 -0300

    criacao da tela campo_minado_app e finalizacao do tabuleiro_test

--- test/models/tabuleiro_test.dart
+++ test/models/tabuleiro_test.dart
@@ -2,5 +2,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:minesweeper/models/tabuleiro.dart';
 
 main() {
-  
+  test('ganhar jogo', () {
+    Tabuleiro tabuleiro = Tabuleiro(linhas: 2, colunas: 2, qtdeBombas: 0);
+
+    tabuleiro.campos[0].minar();
+    tabuleiro.campos[3].minar();
+
+    // Jogando
+    tabuleiro.campos[0].alternarMarcacao();
+    tabuleiro.campos[1].abrir();
+    tabuleiro.campos[2].abrir();
+    tabuleiro.campos[3].alternarMarcacao();
+
+    expect(tabuleiro.resolvido, isTrue);
+  });
 }
